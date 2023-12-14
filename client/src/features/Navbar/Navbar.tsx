7cf560b64e63c16675d43e1cc7dd524fb43836d1
Daniil-ops <daniil252000@mail.ru>
2023-12-14 15:02:06 +0300

    build

---
+++ client/src/features/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function Navbar(): JSX.Element {
+  return (
+    <div>
+      {/* <Link to='/'>Animals</Link>
+        <Link to='/users'>Users</Link>
+        <Link to='/autch/registration' >Rega</Link> */}
+    </div>
+  );
+}
+
+export default Navbar;
