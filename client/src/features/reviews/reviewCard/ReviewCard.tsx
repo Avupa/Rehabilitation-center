5632a45e5b9b8d987fab37d6ab2c41fdbe595f26
Daniil-ops <daniil252000@mail.ru>
2023-12-19 19:18:49 +0300

    fix

--- client/src/features/reviews/reviewCard/ReviewCard.tsx
+++ client/src/features/reviews/reviewCard/ReviewCard.tsx
@@ -7,7 +7,7 @@ type TypeReviewCard = {
 
 function ReviewCard({ review }: TypeReviewCard): JSX.Element {
   function ShortenText(text: string, containerWidth: number, containerHeight: number): string {
-    const fontSize = 20; // Размер шрифта
+    const fontSize = 22; // Размер шрифта
     const averageCharacterWidth = 0.6 * fontSize; // Средняя ширина символа в пикселях
     const averageCharacterHeight = 1.2 * fontSize; // Средняя высота строки в пикселях
     const maxCharactersPerLine = Math.floor(containerWidth / averageCharacterWidth); // Максимальное количество символов в строке
@@ -24,11 +24,21 @@ function ReviewCard({ review }: TypeReviewCard): JSX.Element {
   return (
     <div
       key={review.id}
-      className="container_description border_3px_solid_dark_green width_360px h-96 p-6 mb-5"
+      className="container_description border_3px_solid_dark_green width_365px h-96 p-6 mb-5"
     >
-      <p className="text-2xl mb-2">
-        {review.User.firstName}&nbsp;{review.User.patronymic}
-      </p>
+      <div className="flex justify-between">
+        <p className="mb-2 w-52 h-12">
+          {review.User.firstName} {review.User.patronymic}
+        </p>
+        {Array(5)
+          .fill(null)
+          .map((_, index) => (
+            <div
+              className={`star ${index < review.grade ? 'background_gold' : ''}`}
+            />
+          ))}
+      </div>
+      <p>{review.grade}</p>
       <div>
         <p className="text-lg">{ShortenText(review.description, 360, 200)}</p>
       </div>
